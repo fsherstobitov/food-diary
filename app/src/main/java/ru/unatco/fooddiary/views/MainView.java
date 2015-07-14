29de7f7e89bae5c1e0324d2112c98ff3e32f1852
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-14 13:29:38 +0500

    added MainView interface

---
+++ app/src/main/java/ru/unatco/fooddiary/views/MainView.java
@@ -0,0 +1,13 @@
+package ru.unatco.fooddiary.views;
+
+import java.util.List;
+
+import ru.unatco.fooddiary.data.models.DiaryEntry;
+
+public interface MainView {
+    void showLoadProgress();
+
+    void showEnties(List<DiaryEntry> entries);
+
+    void showErrorMessage();
+}
