8a7ac243e20b668ac5914bc3686b95c18f193abf
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-14 15:27:18 +0500

    added logic for method that implement MainView interface in MainActivity

--- app/src/main/java/ru/unatco/fooddiary/views/activities/MainActivity.java
+++ app/src/main/java/ru/unatco/fooddiary/views/activities/MainActivity.java
@@ -3,10 +3,13 @@ package ru.unatco.fooddiary.views.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,9 +17,14 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import ru.unatco.fooddiary.R;
 import ru.unatco.fooddiary.data.models.DiaryEntry;
+import ru.unatco.fooddiary.presenters.DumbMainPresenter;
+import ru.unatco.fooddiary.presenters.MainPresenter;
 import ru.unatco.fooddiary.views.MainView;
+import ru.unatco.fooddiary.views.adapters.DiaryEntriesListAdapter;
 
 public class MainActivity extends AppCompatActivity implements MainView {
+
+    private MainPresenter mPresenter;
 
     @Bind(R.id.toolbar)
     Toolbar mToolbar;
@@ -24,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements MainView {
     ListView mListView;
     @Bind(android.R.id.progress)
     ProgressBar mProgressBar;
+    @Bind(R.id.error_message)
+    TextView mErrorMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +41,14 @@ public class MainActivity extends AppCompatActivity implements MainView {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         mToolbar.setTitle(R.string.app_name);
+
+        mPresenter = new DumbMainPresenter(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPresenter.loadEntries();
     }
 
     @Override
@@ -57,16 +75,23 @@ public class MainActivity extends AppCompatActivity implements MainView {
 
     @Override
     public void showLoadProgress() {
-
+        mListView.setVisibility(View.GONE);
+        mErrorMessage.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void showEntries(List<DiaryEntry> entries) {
-
+        mErrorMessage.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.GONE);
+        mListView.setAdapter(new DiaryEntriesListAdapter(entries, LayoutInflater.from(this)));
+        mListView.setVisibility(View.VISIBLE);
     }
 
     @Override
     public void showErrorMessage() {
-
+        mListView.setVisibility(View.GONE);
+        mProgressBar.setVisibility(View.GONE);
+        mErrorMessage.setVisibility(View.VISIBLE);
     }
 }
