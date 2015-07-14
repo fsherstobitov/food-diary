0204594a8935f53da1e83178a8ae7ab3cc4ae441
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-14 15:21:37 +0500

    added DiaryEntriesListAdapter with layout

---
+++ app/src/main/java/ru/unatco/fooddiary/views/adapters/DiaryEntriesListAdapter.java
@@ -0,0 +1,69 @@
+package ru.unatco.fooddiary.views.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import ru.unatco.fooddiary.R;
+import ru.unatco.fooddiary.data.models.DiaryEntry;
+
+public class DiaryEntriesListAdapter extends BaseAdapter {
+
+    private static class ViewHolder {
+        @Bind(R.id.date)
+        TextView mDate;
+        @Bind(R.id.note)
+        TextView mNote;
+
+        public ViewHolder(View view) {
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    private final List<DiaryEntry> mEntries;
+    private final LayoutInflater mInflater;
+
+    public DiaryEntriesListAdapter(List<DiaryEntry> entries, LayoutInflater inflater) {
+        mEntries = entries;
+        mInflater = inflater;
+    }
+
+    @Override
+    public int getCount() {
+        return mEntries.size();
+    }
+
+    @Override
+    public DiaryEntry getItem(int i) {
+        return mEntries.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder holder = null;
+        if (view == null) {
+            view = mInflater.inflate(R.layout.item_diary_entry, viewGroup, false);
+            holder = new ViewHolder(view);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        DiaryEntry entry = getItem(i);
+        holder.mDate.setText(entry.getDate().toString());
+        holder.mNote.setText(entry.getNote());
+
+        return view;
+    }
+}
