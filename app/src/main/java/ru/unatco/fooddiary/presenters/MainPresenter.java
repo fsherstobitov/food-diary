259d509f6c500dcb2dbf28d346678f6f419d06d5
Fedor Sherstobitov <fsherstobitov@gmail.com>
2015-07-14 14:40:15 +0500

    removed unnecessary methods from MainPresenter

--- app/src/main/java/ru/unatco/fooddiary/presenters/MainPresenter.java
+++ app/src/main/java/ru/unatco/fooddiary/presenters/MainPresenter.java
@@ -1,11 +1,5 @@
 package ru.unatco.fooddiary.presenters;
 
-import ru.unatco.fooddiary.views.MainView;
-
 public interface MainPresenter {
-    void attachView(MainView mainView);
-
-    void detachView();
-
     void loadEntries();
 }
